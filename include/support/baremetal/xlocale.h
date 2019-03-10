8213bcd12bb3db642bcee501bb32fb937d8a6190
Phillip Johnston <phillip@embeddedartistry.com>
2019-03-10 15:54:46 -0700

    Create a bare metal locale support folder

---
+++ include/support/baremetal/xlocale.h
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP_SUPPORT_BAREMETAL_XLOCALE_H
+#define _LIBCPP_SUPPORT_BAREMETAL_XLOCALE_H
+
+#include <cstdlib>
+#include <clocale>
+#include <cwctype>
+#include <ctype.h>
+#include <support/xlocale/__nop_locale_mgmt.h>
+#include <support/xlocale/__posix_l_fallback.h>
+#include <support/xlocale/__strtonum_fallback.h>
+
+#endif
